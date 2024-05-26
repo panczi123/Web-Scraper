65177917b964da14860c9e1dd2feea93e16b2b5f
panczi123 <130915694+panczi123@users.noreply.github.com>
2024-05-27 01:43:10 +0200

    Update create_csv.py

--- create_csv.py
+++ create_csv.py
@@ -1,8 +1,14 @@
 import pandas as pd
 
-data = {
-    'Company name': ['iZabudowy', 'Bemet'],
-    'Website': ['http://www.izabudowy.pl', 'http://www.bemet.pl/']
-}
-df = pd.DataFrame(data)
-df.to_csv('csv_links.csv', index=False)
+def create_csv():
+    # Create data
+    data = {
+        'Company name': ['iZabudowy', 'Bemet'],
+        'Website': ['http://www.izabudowy.pl', 'http://www.bemet.pl/']
+    }
+
+    # Create a dataframe from the data
+    df = pd.DataFrame(data)
+
+    # Save the dataframe to a .csv file
+    df.to_csv('csv_links.csv', index=False)
